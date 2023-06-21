c3040fa2a9baa49e5b85bf994dda8b607332b1f6
Patricia Eliseu <patyeliseu4@gmail.com>
2023-06-21 17:44:33 -0300

    add req 02 e mudanças no req 1

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,56 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    dish = Dish('pasta', 10.0)
+    assert dish.name == 'pasta'
+    assert dish.price == 10.0
+    assert dish.recipe == {}
+
+    with pytest.raises(TypeError):
+        Dish('pasta', '10.0')
+
+    with pytest.raises(ValueError):
+        Dish('pasta', 0)
+
+    dish1 = Dish('pasta', 10.0)
+    dish2 = Dish('pasta', 10.0)
+    assert dish1 == dish2
+
+    assert dish != 1
+
+    dish1 = Dish('pasta', 10.0)
+    dish2 = Dish('pizza', 15.0)
+    assert dish1 != dish2
+
+    dish1 = Dish('pasta', 10.0)
+    dish2 = Dish('pasta', 10.0)
+    assert hash(dish1) == hash(dish2)
+
+    dish1 = Dish('pasta', 10.0)
+    dish2 = Dish('pizza', 15.0)
+    assert hash(dish1) != hash(dish2)
+
+    farinha = Ingredient('farinha')
+    bacon = Ingredient('bacon')
+
+    dish.add_ingredient_dependency(farinha, 2)
+    dish.add_ingredient_dependency(bacon, 3)
+
+    assert dish.recipe.get(farinha) == 2
+    assert dish.recipe.get(bacon) == 3
+    assert dish.get_ingredients() == {farinha, bacon}
+
+    assert dish.get_restrictions() == farinha.restrictions.union(bacon.restrictions)
+
+    with pytest.raises(TypeError):
+        dish.get_ingredients('InvalidArgument')
+
+        with pytest.raises(TypeError):
+            dish.get_restrictions('InvalidArgument')
+
+        dish_repr = f"Dish('{dish.name}', R${dish.price:.2f})"
+        assert dish_repr == repr(dish)
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -4,28 +4,28 @@ from src.models.ingredient import Restriction
 
 # Req 1
 def test_ingredient():
-    ingredient = Ingredient('farinha de trigo')
-    assert ingredient.name == 'farinha de trigo'
+    ingredient = Ingredient('farinha')
+    assert ingredient.name == 'farinha'
     assert ingredient.restrictions == {Restriction.GLUTEN}
 
     ingredient = Ingredient('unknown_ingredient')
     assert ingredient.name == 'unknown_ingredient'
     assert ingredient.restrictions == set()
 
-    ingredient = Ingredient('farinha de trigo')
-    assert repr(ingredient) == 'Ingredient("farinha de trigo")'
+    ingredient = Ingredient('farinha')
+    assert repr(ingredient) == 'Ingredient("farinha")'
 
-    ingredient1 = Ingredient('farinha de trigo')
-    ingredient2 = Ingredient('farinha de trigo')
+    ingredient1 = Ingredient('farinha')
+    ingredient2 = Ingredient('farinha')
 
-    ingredient1 = Ingredient('farinha de trigo')
-    ingredient2 = Ingredient('ovo')
+    ingredient1 = Ingredient('farinha')
+    ingredient2 = Ingredient('bacon')
     assert not ingredient1 == ingredient2
 
-    ingredient1 = Ingredient('farinha de trigo')
-    ingredient2 = Ingredient('farinha de trigo')
+    ingredient1 = Ingredient('farinha')
+    ingredient2 = Ingredient('farinha')
     assert hash(ingredient1) == hash(ingredient2)
 
-    ingredient1 = Ingredient('farinha de trigo')
-    ingredient2 = Ingredient('ovo')
+    ingredient1 = Ingredient('farinha')
+    ingredient2 = Ingredient('bacon')
     assert hash(ingredient1) != hash(ingredient2)
