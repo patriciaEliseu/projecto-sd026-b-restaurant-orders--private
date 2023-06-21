db919d765b76cc54dae0ad1dcf83b43360d1528c
Patricia Eliseu <patyeliseu4@gmail.com>
2023-06-21 19:22:38 -0300

    ad req 05

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -25,10 +25,30 @@ class InventoryMapping:
     def __init__(self, inventory_file_path=BASE_INVENTORY) -> None:
         self.inventory = read_csv_inventory(inventory_file_path)
 
+    def _load_inventory(self, inventory_path: str) -> Dict[str, int]:
+        inventory = {}
+        with open(inventory_path, "r") as file:
+            for line in file:
+                ingredient, quantity = line.strip().split(",")
+                inventory[ingredient] = int(quantity)
+        return inventory
+
     # Req 5.1
     def check_recipe_availability(self, recipe: Recipe) -> bool:
-        pass
+        def check_recipe_availability(self, recipe: Dict[str, int]) -> bool:
+            for ingredient, required_quantity in recipe.items():
+                if (
+                    ingredient not in self.inventory
+                    or self.inventory[ingredient] < required_quantity
+                ):
+                    return False
+            return True
 
     # Req 5.2
     def consume_recipe(self, recipe: Recipe) -> None:
-        pass
+        def consume_recipe(self, recipe: Dict[str, int]) -> None:
+            if not self.check_recipe_availability(recipe):
+                raise ValueError("Recipe is not available for consumption")
+
+            for ingredient, required_quantity in recipe.items():
+                self.inventory[ingredient] -= required_quantity
